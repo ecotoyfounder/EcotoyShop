14af162feb8d5f5919552b07b30b6386c507129f
Pavel <M3modelist@gmail.com>
2023-02-21 18:51:09 +0200

    first commit

---
+++ client/src/app/pages/good-edit/good-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-good-edit',
+  templateUrl: './good-edit.component.html',
+  styleUrls: ['./good-edit.component.scss']
+})
+export class GoodEditComponent {
+
+}
